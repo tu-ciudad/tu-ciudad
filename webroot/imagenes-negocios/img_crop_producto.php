c2f39105e34ef18e84f1769b8d390cfbe5e04b4b
slyhunt <slyhunt13@gmail.com>
2017-05-10 17:02:09 -0300

    css y calidad de imagen

--- webroot/imagenes-negocios/img_crop_producto.php
+++ webroot/imagenes-negocios/img_crop_producto.php
@@ -19,7 +19,7 @@ $cropH = $_POST['cropH'];
 // rotation angle
 $angle = $_POST['rotation'];
 
-$jpeg_quality = 100;
+$jpeg_quality = 70;
 
 $out = explode(".", $imgUrl);
 //$output_filename = "temp/croppedImg_".rand();

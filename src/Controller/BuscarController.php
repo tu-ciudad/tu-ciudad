0cb0cb7b4635e9dddc1df2544c3f65c800b1d838
Cry4nide <bourgeoistomas@gmail.com>
2017-08-19 11:59:21 -0300

    Buscar funcional
    Subido al host

--- src/Controller/BuscarController.php
+++ src/Controller/BuscarController.php
@@ -1,10 +1,13 @@
 <?php
 namespace App\Controller;
+
 use App\Controller\AppController;
 use Cake\ORM\TableRegistry;
 use Cake\Datasource\EntityInterface;
 use Cake\ORM\Query;
 use Cake\Datasource\ConnectionManager;
+
+
 /**
  * Users Controller
  *
@@ -12,6 +15,7 @@ use Cake\Datasource\ConnectionManager;
  */
 class BuscarController extends AppController
 {
+
     /**
      * Index method
      *
@@ -41,37 +45,25 @@ class BuscarController extends AppController
             $tags = explode(' ', $tags, 4);
             $p = sizeof($tags);
             
+
             for ($i=0; $i < $p; $i++) { 
-                 $tags[$i] = "%".$this->stemmes->stemm($tags[$i])."%";
+                if ($i == 0){
+                 $consulta = "(Productos.cuerpo LIKE '%".$this->stemmes->stemm($tags[$i])."%' or Productos.titulo LIKE '%".$this->stemmes->stemm($tags[$i]."%')");
+                } else {
+                  $consulta = $consulta." AND (Productos.cuerpo LIKE '%".$this->stemmes->stemm($tags[$i])."%' or Productos.titulo LIKE '%".$this->stemmes->stemm($tags[$i]."%')");  
+                }
             }
-            switch($p){
-            case 1: 
-            $variable = $this->paginate($productos->find('all')->contain(['Negocios','ImagenesProductos','Tags'])->innerJoinWith('Tags')->where(['OR' => [['Productos.cuerpo like :tag'],['Productos.titulo like :tag']]])->bind(':tag',$tags[0],'string')->group(['Negocios.nombre','Negocios.telefono','Negocios.direccion','Negocios.descripcion','Negocios.lugares_id','Negocios.perfilfb','Negocios.email','Negocios.users_id','Productos.id'])->order(['Productos.fecha' => 'DESC']));
-            break;
-            case 2:
-            $variable = $this->paginate($productos->find('all')->contain(['Negocios','ImagenesProductos','Tags'])->innerJoinWith('Tags')->
-                where(['AND' => ['OR' => [['Productos.cuerpo like :tag1'],['Productos.titulo like :tag1']]],['OR' => [['Productos.cuerpo like :tag'],['Productos.titulo like :tag']]]])
-                ->bind(':tag',$tags[0],'string')->bind(':tag1',$tags[1],'string')->group(['Negocios.nombre','Negocios.telefono','Negocios.direccion','Negocios.descripcion','Negocios.lugares_id','Negocios.perfilfb','Negocios.email','Negocios.users_id','Productos.id'])->order(['Productos.fecha' => 'DESC']));
-            break;
-            case 3:
-            $variable = $this->paginate($productos->find('all')->contain(['Negocios','ImagenesProductos','Tags'])->innerJoinWith('Tags')->
-                where(['AND' => ['OR' => [['Productos.cuerpo like :tag1'],['Productos.titulo like :tag1']]],['OR' => [['Productos.cuerpo like :tag'],['Productos.titulo like :tag']]],['OR' => [['Productos.cuerpo like :tag2'],['Productos.titulo like :tag2']]]])
-                ->bind(':tag',$tags[0],'string')->bind(':tag1',$tags[1],'string')->bind(':tag2',$tags[2],'string')->group(['Negocios.nombre','Negocios.telefono','Negocios.direccion','Negocios.descripcion','Negocios.lugares_id','Negocios.perfilfb','Negocios.email','Negocios.users_id','Productos.id'])->order(['Productos.fecha' => 'DESC']));
-            break;
-            case 4:
-            $variable = $this->paginate($productos->find('all')->contain(['Negocios','ImagenesProductos','Tags'])->innerJoinWith('Tags')->
-                where(['AND' => ['OR' => [['Productos.cuerpo like :tag1'],['Productos.titulo like :tag1']]],['OR' => [['Productos.cuerpo like :tag'],['Productos.titulo like :tag']]],['OR' => [['Productos.cuerpo like :tag2'],['Productos.titulo like :tag2']]],['OR' => [['Productos.cuerpo like :tag3'],['Productos.titulo like :tag3']]]])
-                ->bind(':tag',$tags[0],'string')->bind(':tag1',$tags[1],'string')->bind(':tag2',$tags[2],'string')->bind(':tag3',$tags[3],'string')->group(['Negocios.nombre','Negocios.telefono','Negocios.direccion','Negocios.descripcion','Negocios.lugares_id','Negocios.perfilfb','Negocios.email','Negocios.users_id','Productos.id'])->order(['Productos.fecha' => 'DESC']));
-            break;
-            default:
-            echo('hola');
-            }
+            $variable = $this->paginate($productos->find('all')->contain(['Negocios','ImagenesProductos','Tags'])->innerJoinWith('Tags')->where($consulta)->group(['Negocios.nombre','Negocios.telefono','Negocios.direccion','Negocios.descripcion','Negocios.lugares_id','Negocios.perfilfb','Negocios.email','Negocios.users_id','Productos.id'])->order(['Productos.fecha' => 'DESC']));
+
                 foreach ($variable as $producto){
                     foreach($producto->imagenes_productos as $imgproducto):
                          $imgproducto->foto = '../../files/ImagenesProductos/'. $imgproducto->foto;
                     endforeach; 
                 }  
+
 /*
+
+
                 $tag = "%".$tag."%";
                $variable = $this->paginate($productos->find('all')->contain(['Negocios','ImagenesProductos','Tags'])->innerJoinWith('Tags')->where(['OR' => [['Tags.nombre like :tag'],['Productos.titulo like :tag']]])->bind(':tag',$tag,'string')->group(['Negocios.nombre','Negocios.telefono','Negocios.direccion','Negocios.descripcion','Negocios.lugares_id','Negocios.perfilfb','Negocios.email','Negocios.users_id','Productos.id'])->order(['Productos.fecha' => 'DESC']));
                 foreach ($variable as $producto){
@@ -82,35 +74,54 @@ class BuscarController extends AppController
         }
         if (isset($this->request->query['comercios'])){
             $comercios = TableRegistry::get('Negocios');
-        $tags = explode(' ',$this->request->query['comercios']);
-            foreach ($tags as $tag){
-                $tag = "%".$tag."%";
-                $variable = $this->paginate($comercios->find('all')->contain(['ImagenesNegocios','Tags'])->innerJoinWith('ImagenesNegocios')->innerJoinWith('Tags')->where(['AND' => [['OR' => [['Tags.nombre like :tag'],['Negocios.nombre like :tag']]],['ImagenesNegocios.ubicacion' => 'portada']]])->bind(':tag',$tag,'string')->group(['Negocios.id','Negocios.nombre','Negocios.telefono','Negocios.direccion','Negocios.descripcion','Negocios.lugares_id','Negocios.perfilfb','Negocios.email','Negocios.users_id']));
+            $tags = preg_replace('[^ A-Za-z0-9_-ñÑ]','', $this->request->query['comercios']);
+            $conectores = array("de", "para", "sin", "si", "con", "co", "no");
+            $tags = str_replace($conectores, "", $tags);
+            $tags = explode(' ', $tags, 4);
+            $p = sizeof($tags);
+            
+
+            for ($i=0; $i < $p; $i++) { 
+                  if ($i == 0){
+                 $consulta = "(Negocios.nombre LIKE '%".$this->stemmes->stemm($tags[$i])."%' or Negocios.descripcion LIKE '%".$this->stemmes->stemm($tags[$i]."%')");
+                } else {
+                  $consulta = $consulta." AND (Negocios.nombre LIKE '%".$this->stemmes->stemm($tags[$i])."%' or Negocios.descripcion LIKE '%".$this->stemmes->stemm($tags[$i]."%')");  
+                }
+            }
+                $variable = $this->paginate($comercios->find('all')->contain(['ImagenesNegocios','Tags'])->innerJoinWith('ImagenesNegocios')->innerJoinWith('Tags')->where(['AND' => [$consulta],['ImagenesNegocios.ubicacion' => 'portada']])->group(['Negocios.id','Negocios.nombre','Negocios.telefono','Negocios.direccion','Negocios.descripcion','Negocios.lugares_id','Negocios.perfilfb','Negocios.email','Negocios.users_id']));
                 foreach($variable as $comercio){
                     foreach ($comercio->imagenes_negocios as $imgcomercio){
                         $imgcomercio->foto = '../../files/ImagenesNegocios/foto/'.$imgcomercio->foto;
                     }
                 }
-            }
             $comercios = 1;
+        }
         }
        
         $this->set(compact('variable','comercios','tags'));
         $this->set('_serialize', ['variable','comercios','tags']);
         }
-    }
-   /*public function categorias(){
+    
+
+   public function categorias(){
         if ($this->request->is('get')){
             $productos = TableRegistry::get('Productos');
              $tags = explode(' ',$this->request->query['categorias']);
-            $variable = $this->paginate($productos->find('all')->contain(['Negocios','ImagenesProductos','Tags'])->innerJoinWith('Tags')->where(['OR' => [['Productos.cuerpo like :tag'],['Productos.titulo like :tag']]])->bind(':tag',$tags[0],'string')->group(['Negocios.nombre','Negocios.telefono','Negocios.direccion','Negocios.descripcion','Negocios.lugares_id','Negocios.perfilfb','Negocios.email','Negocios.users_id','Productos.id'])->order(['Productos.fecha' => 'DESC']));
+                $variable = $productos->find('all')->contain(['Negocios','ImagenesProductos','Tags'])->innerJoinWith('Tags')->where(['Tags.nombre IN' => $tags])->group(['Negocios.nombre','Negocios.telefono','Negocios.direccion','Negocios.descripcion','Negocios.lugares_id','Negocios.perfilfb','Negocios.email','Negocios.users_id','Productos.id'])->order(['Productos.fecha' => 'DESC']);
+
         foreach ($variable as $producto){
                     foreach($producto->imagenes_productos as $imgproducto):
                          $imgproducto->foto = '../../files/ImagenesProductos/'. $imgproducto->foto;
                     endforeach; 
         }  
+
+        $this->set(compact('variable'));
+        $this->set('_serialize', ['variable']);
         }
     }
-*/
+
     //lematizador-------------
+
+
 }
+
